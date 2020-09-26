54519b0870c9298fc94b2b5bcd8dbb9cad5bd6aa
luccalino <61967755+luccalino@users.noreply.github.com>
2020-09-26 10:48:30 +0200

    Delete maps.R

--- code/scripts/maps.R
+++
@@ -1,7 +0,0 @@
-# Loading Rdata file
-load(paste0(paste0(rname,"/export_data/data.Rdata")))
-
-p <- ggplot() +
-  geom_path(data = data, 
-            aes(x = lon, y = lat, group = id),
-            alpha = 0.3, size = 0.2, color = "red", lineend = "round", show.legend = FALSE) 
