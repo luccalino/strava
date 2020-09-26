828058d2d24188c047b49824714beadd656875d7
luccalino <61967755+luccalino@users.noreply.github.com>
2020-09-26 11:24:45 +0200

    Update makefile.R

--- code/makefile.R
+++ code/makefile.R
@@ -1,19 +1,21 @@
 # Clear environment to start from a white page
 remove(list = ls())
 
+############################# Adjust only this part ################################ 
 # Set working directory (Your working directory!)
-setwd("/Users/laz/Library/Mobile Documents/com~apple~CloudDocs/Projects/strava/data/friends/")
+setwd("YOUR_WORKING_DIR")
 
 # Select variables
-rname = "Your_name" 
+rname = "YOUR_NAME" 
 country = "schweiz" #"italien" #"frankreich"
-python_path = "/Users/laz/opt/anaconda3/bin/python" # Your path where the python executable is located
+python_path = "YOUR_PYTHON_PATH" # Your path where the python executable is located
 
 # Table of contents (1: Execute; 0: Chill...)
 import = 0 # Data import
 ridges = 0 # Plots ridges
 passed = 0 # Plots Pass Checkbook 
 scrape = 0 # Scrape Pass Info from quäldich.de
+####################################################################################
 
 # Execute scripts
 if (import == 1) {
